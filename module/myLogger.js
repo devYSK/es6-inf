a1efc2f8d2b2861220bca86c1359fbc8ed5e718a
Young Soo Kim <41531594+devYSK@users.noreply.github.com>
2021-08-06 14:38:20 +0900

    export import 서비스 코드 구현 학습

--- module/myLogger.js
+++ module/myLogger.js
@@ -1,3 +1,23 @@
 export default function log(data) {
     console.log(data);
 }
+
+
+const getCurrentHour = () => {
+    return (new Date).getHours();
+}
+
+class MyLogger {
+    constructor(props) {
+        this.lectures = ['java', 'ios'];
+
+    }
+
+    getLectures() {
+        return this.lectures;
+    }
+
+    getTime = () => {
+        return Date.now();
+    }    
+}
